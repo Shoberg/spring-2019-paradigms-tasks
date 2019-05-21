47b467207a36e94dc823ed04f8fdc7886919fc68
Shoberg <49321396+Shoberg@users.noreply.github.com>
2019-05-22 05:20:15 +1000

    Update NaiveTree.hs

--- task11/src/NaiveTree.hs
+++ task11/src/NaiveTree.hs
@@ -8,9 +8,7 @@ import Map
 
 {-|
   Двоичное дерево поиска, необязательно сбалансированное.
-
   Инвариант: для любой вершины @v@:
-
   1. Все ключи в левом поддереве строго меньше ключа @v@.
   2. Все ключи в правом поддереве строго больше ключа @v@.
 -}
@@ -27,11 +25,11 @@ data NaiveTree k a =
   что все ключи из @l@ строго меньше ключей из @r@.
 -}
 merge :: NaiveTree k a -> NaiveTree k a -> NaiveTree k a
-merge = undefined
+merge Nil              r = r
+merge (Node k v ll rl) r = Node k v ll (merge rl r)
 
 {-|
   Реализация функций 'Map' для 'NaiveTree'.
-
   'empty', 'singleton' и 'Map.null' работают за /O(1)/.
   Если /n/ – количество вершин дерева, а /h/ – высота дерева,
   то 'fromList' работает за /O(nh)/, 'toAscList' работает за /O(n^2)/,
@@ -39,20 +37,32 @@ merge = undefined
   Остальные функции работают за /O(h)/,
   причём каждая функция должна спускаться вниз по дереву и
   подниматься обратно не больше одного раза.
-
   Скорее всего, при реализации вам потребуется функция 'merge'.
 -}
 instance Map NaiveTree where
-    empty = undefined
+    empty = Nil
 
-    singleton = undefined
+    singleton k v = Node k v Nil Nil 
 
-    toAscList = undefined
+    toAscList Nil            = []
+    toAscList (Node k v l r) = toAscList l ++ [(k, v)] ++ toAscList r
 
-    alter = undefined
+    alter f k' Nil                     = 
+        case f Nothing of Nothing  -> Nil
+                          Just val -> Node k' val Nil Nil
+    alter f k' (Node k v l r) | k' < k = Node k v (alter f k' l) r
+    alter f k' (Node k v l r) | k' > k = Node k v l (alter f k' r)
+    alter f k' (Node _ v l r)          = 
+        case f $ Just v of Nothing  -> merge l r
+                           Just val -> Node k' val l r
 
-    lookup = undefined
+    lookup _  Nil                     = Nothing
+    lookup k' (Node k _ l _) | k' < k = Map.lookup k' l
+    lookup k' (Node k _ _ r) | k' > k = Map.lookup k' r
+    lookup _  (Node _ v _ _)          = Just v
 
-    null = undefined
+    null Nil = True
+    null _   = False
 
-    size = undefined
+    size Nil            = 0
+    size (Node _ _ l r) = size l + 1 + size r
