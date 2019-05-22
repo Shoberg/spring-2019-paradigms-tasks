75b87ea365690e6b4ed6396f6bf86f7646ed5f04
Shoberg <49321396+Shoberg@users.noreply.github.com>
2019-05-22 20:18:20 +1000

    Add files via upload

---
+++ task09/src/tsqueue.cpp
@@ -0,0 +1,30 @@
+#include "tsqueue.h"
+
+void threadsafe_queue_init(ThreadsafeQueue *q) {
+    queue_init(&q->q);
+    pthread_mutex_init(&q->mutex, NULL);
+    pthread_cond_init(&q->cond_nonempty, NULL);
+}
+
+void threadsafe_queue_destroy(ThreadsafeQueue *q) {
+    pthread_cond_destroy(&q->cond_nonempty);
+    pthread_mutex_destroy(&q->mutex);
+    queue_destroy(&q->q);
+}
+
+void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
+    pthread_mutex_lock(&q->mutex);
+    queue_push(&q->q, data);
+    pthread_cond_signal(&q->cond_nonempty);
+    pthread_mutex_unlock(&q->mutex);
+}
+
+void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
+    pthread_mutex_lock(&q->mutex);
+    while (queue_empty(&q->q)) {
+        pthread_cond_wait(&q->cond_nonempty, &q->mutex);
+    }
+    void *ret = queue_pop(&q->q);
+    pthread_mutex_unlock(&q->mutex);
+    return ret;
+}
